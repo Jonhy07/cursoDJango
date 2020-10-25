ee3422fa39a025eabbcc427c403b60171d5ab00a
Juan <jonhy.simaj@gmail.com>
2020-10-25 02:15:00 -0600

    tercer subida}

--- MiProyecto/MiProyecto/settings.py
+++ MiProyecto/MiProyecto/settings.py
@@ -32,8 +32,8 @@ ALLOWED_HOSTS = ['127.0.0.1', '.pythonanywhere.com']
 # Application definition
 
 INSTALLED_APPS = [
-    'jet.dashboard',
-    'jet',
+    #'jet.dashboard',
+    #'jet',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
